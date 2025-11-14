33b176c01fdab0c4df345ff3d8f416b9740c32f2
madiha <madihakanwal655@gmail.com>
2025-11-14 06:14:00 +0500

    migrations

---
+++ database/migrations/2025_11_13_044121_create_coupons_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('coupons', function (Blueprint $table) {
+            $table->id();
+ $table->string("code");
+  $table->string("discount_type");
+   $table->decimal("discount_value", 10, 2);
+    $table->date("star_date");
+     $table->date("end-date");
+
+
+      $table->decimal("min_order_amount", 10, 2)->default(0);
+      $table->integer("max_uses")->default(0);
+      $table->boolean("status")->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('coupons');
+    }
+};
