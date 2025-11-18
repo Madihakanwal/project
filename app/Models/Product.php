98b0530c5f0193593511b8327369194ce20c42da
madiha <madihakanwal655@gmail.com>
2025-11-18 12:25:52 +0500

    product casts

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,16 +8,38 @@ class Product extends Model
 {
     protected $fillable = [
         'category_id',
-    'sub_category_id',
-    'name',
-    'slug',
-    'sku',
-    'price',
-    'discount_price',
-    'stock',
-    'short_description',
-    'long_description',
-    'status', 
-    'featured',
+        'sub_category_id',
+        'name',
+        'slug',
+        'sku',
+        'price',
+        'discount_price',
+        'stock',
+        'short_description',
+        'long_description',
+        'status',
+        'featured',
+    ];
+
+    protected $casts = [
+        'category_id'     => 'integer',
+        'sub_category_id' => 'integer',
+
+        'name'            => 'string',
+        'slug'            => 'string',
+        'sku'             => 'string',
+
+        'price'           => 'decimal:2',
+        'discount_price'  => 'decimal:2',
+        'stock'           => 'integer',
+
+        'short_description' => 'string',
+        'long_description'  => 'string',
+
+        'status'          => 'boolean', // active/inactive
+        'featured'        => 'boolean', // true/false for featured products
+
+        'created_at'      => 'datetime',
+        'updated_at'      => 'datetime',
     ];
 }
